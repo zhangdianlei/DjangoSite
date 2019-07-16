1e37e1ac536b503c1808865c2090464d54be4e53
zhangdianlei <dianlei.zhang@gmail.com>
2019-07-16 16:57:55 +0800

    http404

--- polls/views.py
+++ polls/views.py
@@ -21,7 +21,12 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("you are looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question dose not exit")
+
+    return render(request, 'polls/detail.html', {'question':question})
 
 
 def results(request, question_id):
