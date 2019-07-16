1d10bf075838fe5d63d6fc543a7d5a6280d0d6b7
zhangdianlei <dianlei.zhang@gmail.com>
2019-07-16 17:00:16 +0800

    test

--- polls/views.py
+++ polls/views.py
@@ -6,8 +6,6 @@ from django.shortcuts import render
 from django.http import Http404
 
 
-# Create your views here.
-
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     template = loader.get_template('polls/index.html')
