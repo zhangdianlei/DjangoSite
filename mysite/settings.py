7514785ff4e752c6a20544d46acd6acf2c8e8b08
zhangdianlei <dianlei.zhang@gmail.com>
2019-07-17 17:24:10 +0800

    add html

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'jz)jofosy2iq$b%as2%0*5k#**j&2_om()6(#!r!7vnx9#cgnk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Question
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 
 
@@ -18,11 +18,17 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question dose not exit")
+#
+#     return render(request, 'polls/detail.html', {'question':question})
+
+
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question dose not exit")
+    question = get_object_or_404(Question, pk=question_id)
 
     return render(request, 'polls/detail.html', {'question':question})
 
