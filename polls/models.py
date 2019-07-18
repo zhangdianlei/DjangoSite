9045bed7354b959c36db9a29350cea5bb2ae6111
zhangdianlei <dianlei.zhang@gmail.com>
2019-07-18 09:45:23 +0800

    add vote method

--- polls/models.py
+++ polls/models.py
@@ -24,3 +24,4 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from .models import Question
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
+from django.urls import reverse
 
 
 def index(request):
@@ -34,9 +35,21 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "you are looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
-    return HttpResponse("you are voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {'question':question, 'error_message':'You did not select a choice'})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
